5c767150a510d25652d93dc846ac99550e2808a2
Natia Meskhi <natia.meskhi.natia@gmail.com>
2021-03-15 18:24:00 +0100

    vue.js app

---
+++ app.js
@@ -0,0 +1,25 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      firstName: "Orion",
+      lastName: "Meskhi",
+      email: "orion@gmail.com",
+      picture: "https://randomuser.me/api/portraits/men/10.jpg",
+    };
+  },
+  methods: {
+    async getUser() {
+      const res = await fetch("https://randomuser.me/api");
+      const { results } = await res.json();
+
+      // console.log(results)
+
+      this.firstName = results[0].name.first;
+      this.lastName = results[0].name.last;
+      this.email = results[0].email;
+      this.picture = results[0].picture.large;
+    },
+  },
+});
+
+app.mount("#app");
